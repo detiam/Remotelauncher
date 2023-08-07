e1937bf480dfd74a43236d1f164c5dbe797c5109
detiam <dehe_tian@outlook.com>
2023-08-07 22:42:04 +0800

    详情页面 引入autosize处理textarea

--- remotelauncher/static/js/page-detail.js
+++ remotelauncher/static/js/page-detail.js
@@ -26,26 +26,28 @@ if (params.get('need_reload') === '1') {
 };
 
 // https://stackoverflow.com/questions/454202/creating-a-textarea-with-auto-resize
-$(document).on('input mouseenter mouseleave', 'textarea', function () {
-  $(this).outerHeight(36).outerHeight(this.scrollHeight);
+$(document).on('click','button#edit-program-btn',()=>{
+  autosize.update(document.querySelectorAll('textarea.autosize'))
+});
+
+document.addEventListener('keydown', function (event) {
+  // 如果按下的是Ctrl + S, 则触发按钮的单击事件
+  if (event.ctrlKey && event.code === 'KeyS') {
+    event.preventDefault(); // 防止默认的保存操作
+    document.getElementById('detail-save-button').click(); // 触发按钮的单击事件
+  }
+  if (event.code === 'Escape') {
+    // event.preventDefault();
+    window.close()
+  }
 });
 
 document.addEventListener('DOMContentLoaded', () => {
+  autosize(document.querySelectorAll('textarea.autosize'))
   // 显示警告框
   $("#alert").fadeIn();
   // 定时隐藏警告框
   setTimeout(function () {
     $("#alert").fadeOut();
   }, 3000); // 设置5秒后自动隐藏
-  document.addEventListener('keydown', function (event) {
-    // 如果按下的是Ctrl + S, 则触发按钮的单击事件
-    if (event.ctrlKey && event.code === 'KeyS') {
-      event.preventDefault(); // 防止默认的保存操作
-      document.getElementById('detail-save-button').click(); // 触发按钮的单击事件
-    }
-    if (event.code === 'Escape') {
-      // event.preventDefault();
-      window.close()
-    }
-  });
 });
